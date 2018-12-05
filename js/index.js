608e8de64de1a4e348c97ab051dfdaf8c1865e74
L-ALICE <lalicetvt@163.com>
2018-12-05 09:21:15 +0800

    注册页静态完成

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 
 window.onload = function(){
 	var index = 0;
-	var aa = new autoplay();
+	var aa = new Autoplay();
 	aa.init();
+	var bb = new Tj();
+	bb.init();
 	//导航栏操作
 	var $oo = $("#nav ul li"),
 	    $olist = $(".pao"),
@@ -15,6 +17,7 @@ window.onload = function(){
 					$oo.eq(index).css('background',"");
 				})
 		 $olist.hover(function(){
+			 console.log(1);
 					var index = $(this).index()-1;
 					var flag = true;
 					//根据下标操作对应的内容
@@ -68,7 +71,7 @@ window.onload = function(){
 // 				$dc.css("left","-$dw*index");
 // 			}
 			//console.log($dc.css())
-			 function autoplay(){
+			 function Autoplay(){
 				 this.dc = document.getElementsByClassName("dc");
 				 this.dc1 = document.getElementsByClassName("dc1");
 				 this.dWidth = this.dc1[0].offsetWidth;
@@ -95,9 +98,89 @@ window.onload = function(){
 				 console.log(this.dWidth,index)
 				 }   		 	 
 			}	
-			 
-			 
+			 //推荐轮播
+			 function Tj(){
+			 	//h2所对应的div
+			 	this.tui = document.getElementById("tui");
+			 	//5000px的div
+			 	this.tbi = document.getElementById("tbi");
+			 	//图片对应的div
+			 	this.bs = document.getElementsByClassName("bs");
+			 	//console.log(this.bs);
+			 	//左按钮
+			 	this.leftBtn = document.getElementById("btn_l");
+			 	//右按钮
+			 	this.rightBtn = document.getElementById("btn_r");
+			 	index = 0,
+			 	time = null ;
+			 	this.init = function(){
+			 		this.row();
+			 		this.row1();
+			 		this.pic();
+			 		this.slide();
+			 	
+			 	}
+			 //自动轮播
+																		 this.slide = function(){
+																			 var left = 0;													var time = setInterval(function(){
+						left --;													this.tbi.style.left = left + "px";								if(left < -1650){
+						  left = 0;														 }													 }.bind(this),10) 
 			}
-		
+			  //鼠标操作图片 移入 高亮 离开 恢复透明度50
+				 this.pic = function(){
+					 for(let i = 0 ; i < this.bs.length ; i ++){
+						 this.bs[i].onmouseenter = function(){
+							 startMove(this.bs[i],{opacity:100});
+							
+						 }.bind(this);
+						 this.bs[i].onmouseleave = function(){
+							 if(index != i){
+								 startMove(this.bs[i],{opacity:50});
+							 }
+							 
+						 }.bind(this);
+					 }
+				 }
+				 //鼠标移入移出箭头显示
+				 this.row = function(){
+					 this.tbi.onmouseenter = this.leftBtn.onmouseenter = this.rightBtn.onmouseenter = function(){
+					 startMove(this.leftBtn,{opacity:100});
+					 startMove(this.rightBtn,{opacity:100}); 
+					 }.bind(this);
+				 }
+				 //鼠标移出箭头隐藏
+				 this.row1 = function(){
+					 this.tbi.onmouseleave = function(){
+						startMove(this.leftBtn,{opacity:0});
+						startMove(this.rightBtn,{opacity:0});  
+					 }.bind(this);
+				 }
+				 //点击左侧箭头 index--
+				 this.left = function(){
+					 this.leftBtn.onclick = function(){
+						 if(target == 0){
+							 target = 0;
+						 }else{
+							 target += 330;
+						 }
+						 startMove(this.tbi,{left:target});
+					 }.bind(this);
+				 }
+				 //点击右侧箭头 index ++
+				 this.right = function(){
+					 this.rightBtn.onclick = function(){
+					 	if(target == -330*10){
+					 		target = -330*10;
+					 	}else{
+					 		target -= 330;
+					 	}
+					 	startMove(this.tbi,{right:target});
+					 }.bind(this);
+				 }
+				 
+			 }
+			 
+			
+		}
 		
 
